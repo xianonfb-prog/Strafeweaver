6eae4f55a80dc904091f53e4f677be863c2f6b6d
xianonfb-prog <xianonfb@gmail.com>
2026-07-18 21:58:15 +0800

    Implement StrafeweaverPlugin with event and command registration

--- StrafeweaverPlugin.java
+++ StrafeweaverPlugin.java
@@ -1,1 +1,36 @@
+package com.yourserver.strafeweaver;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StrafeweaverPlugin extends JavaPlugin {
+
+    private static StrafeweaverPlugin instance;
+    private StrafeweaverManager manager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        
+        // Initialize the manager (handles item creation and unique tracking)
+        manager = new StrafeweaverManager(this);
+        
+        // Register Events and Commands
+        getServer().getPluginManager().registerEvents(new StrafeweaverListener(this, manager), this);
+        getCommand("strafeweaver").setExecutor(new StrafeweaverCommand(this, manager));
+        
+        getLogger().info("Strafeweaver Plugin Enabled! The hunt begins...");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Strafeweaver Plugin Disabled.");
+    }
+
+    public static StrafeweaverPlugin getInstance() {
+        return instance;
+    }
+    
+    public StrafeweaverManager getManager() {
+        return manager;
+    }
+}
